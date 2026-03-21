318eaaff63f2d1834b363c75dbd4e7f2156a150f
loozepresents-design <loozepresent@gmail.com>
2026-03-22 02:10:05 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,13 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running");
+});
+
+app.get("/", (req, res) => {
+  res.send("OK");
 });
 
 // ===== Discord Bot =====
